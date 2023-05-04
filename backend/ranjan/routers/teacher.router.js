4d23aca8c1ee9e5f8ea81557ca3e9a8a87301b98
Ranjan Sahu <sahu.ranjanrn@gmail.com>
2023-05-04 21:27:34 +0530

    day1

---
+++ backend/ranjan/routers/teacher.router.js
@@ -0,0 +1,44 @@
+const { TeacherModel } = require("../models/teacher.model");
+const teacherRouter = require("express").Router();
+
+//get all teacher
+teacherRouter.get("/",async(req,res)=>{
+  res.send("welcome")
+})
+teacherRouter.get("/allteacher", async (req, res) => {
+    const Teachers = await TeacherModel.find()
+    res.send(Teachers)
+  });
+
+  teacherRouter.post("/addteacher", async (req, res) => {
+    let {
+      teacherName,
+      email,
+      qualifications,
+      experience,
+      phoneNo,
+      city,
+      subject,
+      image,
+    } = req.body;
+    try {
+      let teacher = new TeacherModel({
+        teacherName,
+        email,
+        qualifications,
+        experience,
+        phoneNo,
+        city,
+        subject,
+        image,
+      });
+      await teacher.save();
+      res.status(201).send({ msg: "teacher has been created", teacher });
+    } catch (error) {
+      res
+        .status(500)
+        .send({ msg: "Error in created teacher" });
+    }
+  });
+
+module.exports = {teacherRouter};
