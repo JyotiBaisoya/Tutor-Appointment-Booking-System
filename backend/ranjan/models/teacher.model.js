f75b8d107f6bf16bc99a390bb2c7a0ee6ed4e1c8
Punit Juneja <punitjuneja123@gmail.com>
2023-05-08 13:50:24 +0530

    appointment page

--- backend/ranjan/models/teacher.model.js
+++ backend/ranjan/models/teacher.model.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 
-const teacherSchema = mongoose.Schema({
-    teacherName: {type: String,required: true,},
+const teacherSchema = mongoose.Schema(
+  {
+    teacherName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    qualifications: {type: String,required: true,},
-    experience: {type: String,required: true,},
-    phoneNo: {type: Number,required: true,unique: true,},
-    city: {type: String,required: true,},
-    subject: {type: String,required: true,},
-    image: {type: String,},
-    about:{type: String},
-  },{versionKey:false});
-  
-  const TeacherModel = mongoose.model("Teachers", teacherSchema);
-  
-  module.exports = {
-    TeacherModel,
-  };
-  
+    qualifications: { type: String, required: true },
+    experience: { type: String, required: true },
+    phoneNo: { type: Number, required: true, unique: true },
+    city: { type: String, required: true },
+    subject: { type: String, required: true },
+    image: { type: String },
+    about: { type: String },
+    slots: [{ studentID: String, date: String, time: String }],
+  },
+  { versionKey: false }
+);
+
+const TeacherModel = mongoose.model("Teachers", teacherSchema);
+
+module.exports = {
+  TeacherModel,
+};
